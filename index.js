e119ac7321a8021c056613b22ef80c43ba3839ae
paola-barbolla <44746783+paola-barbolla@users.noreply.github.com>
2021-02-23 16:33:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const util = require("util")
+
+//lettura asincrona
+fs.readFile("prova.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(errore)
+    } else {
+        console.log(data)
+    }
+})
+
+//lettura sincrona
+let file = fs.readFileSync("prova1.txt", "utf-8")
+console.log(file)
+
+//lettura asincrona con promisify
+const readFile = util.promisify(fs.readFile)
+function lettura() {
+    return readFile("prova2.txt", "utf-8")
+}
+lettura().then(data => {
+    console.log(data)
+})
+
+console.log("ciao")
